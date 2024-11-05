f52a8136cfce233bf1170b7b1db61193d6da7870
Luis G <luisdavidnazareth@gmail.com>
2024-11-05 17:58:16 -0300

    modifico controladores

--- controllers/cities/update.js
+++ controllers/cities/update.js
@@ -4,8 +4,8 @@ const updateI = async (req, res, next) => {
     try {
         let city = req.body
         await City.updateOne(
-            {_id: city._id},//condiciones de busqueda
-            {name: city.name} // modificaciones 
+            {_id: city._id},
+            {name: city.name} 
         )
         return res.status(200).json({
             response: city
